5cd0da45fa8272f4875038e678e46c26a23e8412
Rohit Bajaj <rbajaj1402@gmail.com>
2020-07-22 23:30:01 +0530

    Preparing for Heroku build v2.0

--- server.js
+++ server.js
@@ -82,9 +82,9 @@ io.on('connection', (socket) => {
 const port = process.env.PORT || 8090;
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('../client/build'));
+    app.use(express.static('client/build'));
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
